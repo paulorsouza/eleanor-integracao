f88f38956b555555aa6a2420d066325552ece86f
Paulo Ricardo de Souza <paulor1809@gmail.com>
2018-05-18 17:37:19 -0300

    Fix

--- api/src/main/kotlin/br/com/eleanor/controller/IntegracaoController.kt
+++ api/src/main/kotlin/br/com/eleanor/controller/IntegracaoController.kt
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework
 
 @RestController
 @CrossOrigin(origins = arrayOf("http://localhost", "http://localhost:3000", "http://localhost:8080", "http://192.168.254.91", "http://192.168.254.91:80"),
