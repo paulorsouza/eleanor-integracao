93b84eeb8f3e28fb38112ed484b0d0231a030c2e
Paulo Ricardo de Souza <paulor1809@gmail.com>
2018-05-18 18:35:27 -0300

    Agora vai

--- api/src/main/kotlin/br/com/eleanor/controller/IntegracaoController.kt
+++ api/src/main/kotlin/br/com/eleanor/controller/IntegracaoController.kt
@@ -10,15 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin(origins = arrayOf("http://192.168.0.5:8080", "http://192.168.0.5", "http://192.168.254.91:8080"))
 @RestController
-@CrossOrigin(origins = arrayOf("http://localhost", "http://localhost:3000", "http://localhost:8080", "http://192.168.254.91", "http://192.168.254.91:80", "http://192.168.254.91/eleanor"),
-        allowedHeaders=arrayOf("Origin", "Content-Type"),
-        exposedHeaders=arrayOf("Origin", "Content-Type"),
-        allowCredentials="false")
 @RequestMapping("/integracao")
 class IntegracaoController {
     val oracleTemplate = NamedParameterJdbcTemplate(HikariCustomConfig().getOracleTemplate())
-
+    
     @GetMapping("/firebase/pedidos/integrar", produces = arrayOf("application/json"))
     fun integrarPedidos() : List<IntegracaoResultData> {
         val integracao = IntegracaoFirebase(oracleTemplate)
--- update-app.sh
+++ update-app.sh
@@ -1,6 +1,7 @@
 echo "Atualizando eleanor integracao"
 echo "----------------------------------------------------------------"
 sudo systemctl stop apache2
+sudo docker ps -a | awk '{ print $1,$2 }' | grep eleanor | awk '{print $1 }' | xargs -I {} sudo docker rm -f {}
 git pull
 cd ui
 npm install
@@ -9,7 +10,6 @@ cd ..
 cd api
 export PATH=$PATH:/opt/gradle/gradle-4.7/bin
 gradle build
-sudo docker ps -a | awk '{ print $1,$2 }' | grep eleanor | awk '{print $1 }' | xargs -I {} sudo docker rm -f {}
 cd ..
 sudo docker build --rm -t eleanor . 
-sudo docker run -p 80:8080 eleanor &
+sudo docker run -p 8080:8080 eleanor &
