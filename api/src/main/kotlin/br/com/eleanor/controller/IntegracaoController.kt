1747bf213e73b162d12ee9d69f182305c072e7b8
Paulo Ricardo de Souza <paulor1809@gmail.com>
2018-05-18 19:51:00 -0300

    Remove cors

--- api/src/main/kotlin/br/com/eleanor/controller/IntegracaoController.kt
+++ api/src/main/kotlin/br/com/eleanor/controller/IntegracaoController.kt
@@ -5,12 +5,10 @@ import br.com.eleanor.databases.HikariCustomConfig
 import br.com.eleanor.rules.IntegracaoFirebase
 import br.com.eleanor.rules.IntegracaoOracle
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-@CrossOrigin(origins = arrayOf("http://192.168.254.91:8080"))
 @RestController
 @RequestMapping("/integracao")
 class IntegracaoController {
